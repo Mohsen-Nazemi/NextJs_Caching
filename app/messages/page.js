d923f1d66006225a0eb71ad3af1f4028dc3ec430
Mohsen-Nazemi <programmer.mohsen@gmail.com>
2024-06-18 13:41:06 +0330

    controlling data caching

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,11 +1,13 @@
+import { unstable_noStore } from "next/cache";
 import Messages from "@/components/messages";
 
+export const revalidate = 5;
+// export const dynamic = "force-dynamic"; is equal with cache: 'no-store'
+// export const dynamic = "force-static";
+
 export default async function MessagesPage() {
-  const response = await fetch("http://localhost:8080/messages", {
-    next: {
-      revalidate: 20,
-    },
-  });
+  unstable_noStore();
+  const response = await fetch("http://localhost:8080/messages");
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
